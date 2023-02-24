85521b949212a52315e79c01c171591cf6012aa7
rojinalya <42582400+rojinalya@users.noreply.github.com>
2023-02-24 22:12:01 +0200

    Update display.c

--- display.c
+++ display.c
@@ -82,8 +82,8 @@ static void prvDISP_Task(void * pvParameters)
     {
         /* Wait for message from meter task  */
         /* Add your code here! */
-    	while(!(uxQueueMessagesWaiting(qh)));
-    	xQueueReceive(qh,&message,0);
+    	while(!(uxQueueMessagesWaiting(xMET2DISP_Queue)));
+    	xQueueReceive(xMET2DISP_Queue,&message,0);
         /* End of your code! */
 
         if (message.type == MET2DISP_UpdateWindow)
